c1cfdb160cb05c855f94de5cd7ef341371d7785e
Kauealbr <kauealbr@gmail.com>
2026-01-18 16:06:07 -0300

    Cria classe Cliente

--- models.py
+++ models.py
@@ -7,8 +7,16 @@ class Produto:
 
     def __str__(self):
             return f"ID: {self.id} | {self.nome} | R$ {self.preco:.2f} | Estoque: {self.quantidade}" 
+    
+class Cliente:
+    def __init__(self, id, nome, email, telefone):
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+
 
 
 if __name__ == "__main__":
-    p = Produto(1, "Arroz", 25.9, 10)
-    print(p)
+    c = Cliente(1, "João", "joao@email.com", "11999999999")
+    print(c.nome, c.email, c.telefone)
